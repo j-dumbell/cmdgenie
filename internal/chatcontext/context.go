6ad53aa110bfc51b47da7b4d81535f04c8fe4052
James Dumbell <james.dumbell@grafana.com>
2025-03-11 23:32:22 +0000

    add OS context

--- internal/chatcontext/context.go
+++ internal/chatcontext/context.go
@@ -1,15 +1,16 @@
 package chatcontext
 
-const Minimal = "You are an AI assistant that translates natural language prompts into precise shell commands. " +
-	"Your responses should contain only the command itself, without explanations or additional text. " +
-	"Any commands should not be returned in code blocks" +
-	"Assume the user is running commands in a Unix-like shell (e.g. Zsh, Bash). " +
-	"Use commonly available command-line tools unless otherwise specified. " +
+import (
+	"fmt"
+	"runtime"
+	"slices"
+)
+
+const minimalContext = "Your responses should contain only the command itself, without explanations or additional text. " +
 	"If multiple commands are required, provide them on separate lines."
 
-const Verbose = "You are an AI assistant that translates natural language prompts into shell commands, and you should provide detailed responses that include explanations. " +
+const verboseContext = "You should provide detailed responses that include explanations. " +
 	"For each shell command you generate, explain what the command does and how it works. " +
-	"Assume the user is running commands in a Unix-like shell (e.g., Bash). " +
 	"The response should display nicely in the terminal." +
 	"Your response should include:\n\n" +
 	"The shell command itself.\n" +
@@ -17,3 +18,48 @@ const Verbose = "You are an AI assistant that translates natural language prompt
 	"A breakdown of key flags or options used in the command, if applicable.\n" +
 	"Any prerequisites or assumptions made (e.g., AWS CLI is installed).\n" +
 	"Do not provide just the command; always include an explanation of how to use it."
+
+var verbosityContext = map[Verbosity]string{
+	VerbosityMinimal: minimalContext,
+	VerbosityVerbose: verboseContext,
+}
+
+type Verbosity int
+
+const (
+	VerbosityMinimal Verbosity = 0
+	VerbosityVerbose Verbosity = 1
+)
+
+type OS string
+
+const (
+	OSDarwin  OS = "darwin"
+	OSLinux   OS = "linux"
+	OSWindows OS = "windows"
+)
+
+func BuildContext(getOS OSGetter, verbosity Verbosity) string {
+	contextOS := OSLinux
+	systemOS := getOS()
+	if slices.Contains([]OS{OSDarwin, OSLinux, OSWindows}, OS(systemOS)) {
+		contextOS = OS(systemOS)
+	}
+
+	return fmt.Sprintf(
+		"You are an AI assistant that translates natural language prompts into precise shell commands. "+
+			"Use commonly available command-line tools unless otherwise specified. "+
+			"Assume the user is running commands on the %s operating system. "+
+			"%s",
+		contextOS,
+		verbosityContext[verbosity],
+	)
+}
+
+var allowedOS = []string{"darwin", "linux", "windows"}
+
+type OSGetter func() string
+
+func GetOS() string {
+	return runtime.GOOS
+}
